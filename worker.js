d71c34cfa14c2d376481b20e6d46cc14a087dc7b
Leorium <contact@leorium.com>
2025-06-05 11:18:32 +0800

    Explicitly add favicon to worker.js

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@ import { serveStatic } from "@hono/node-server/serve-static";
 import { Hono } from "hono";
 
 const app = new Hono();
-
+app.get('/favicon.ico', serveStatic({ path: './public/favicon.ico' }));
 app.use("/*", serveStatic({ root: "./public" }));
 
 export default app;
